9d428ca18b369227b5e12e8e25021e9033b3d580
Kacper Harasim <kacper.harasim@polidea.com>
2016-04-15 09:45:02 +0200

    Some work on Peripheral documentation.

---
+++ Example/Example/CharacteristicsTableViewController.swift
@@ -0,0 +1,11 @@
+//
+//  CharacteristicsTableViewController.swift
+//  Example
+//
+//  Created by Kacper Harasim on 14.04.2016.
+//  Copyright © 2016 Polidea. All rights reserved.
+//
+
+import Foundation
+
+c
