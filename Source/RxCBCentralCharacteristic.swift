4bb2d7e3c4ba3ad79697325a3083767474d87137
Andrew Breckenridge <asbreckenridge@me.com>
2016-06-20 17:24:24 -0700

    create MutableCharacteristic

---
+++ Source/RxCBCentralCharacteristic.swift
@@ -0,0 +1,44 @@
+//
+//  RxCBCentralCharacteristic.swift
+//  RxBluetoothKit
+//
+//  Created by Andrew Breckenridge on 6/20/16.
+//  Copyright © 2016 Polidea. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+class RxCBMutableCharacteristic: RxMutableCharacteristicType {
+    
+    let characteristic: CBMutableCharacteristic
+    
+    init(characteristic: CBMutableCharacteristic) {
+        self.characteristic = characteristic
+    }
+    
+    var uuid: CBUUID {
+        return characteristic.UUID
+    }
+    var value: NSData? {
+        return characteristic.value
+    }
+    var isNotifying: Bool {
+        return characteristic.isNotifying
+    }
+    var properties: CBCharacteristicProperties {
+        return characteristic.properties
+    }
+    var descriptors: [RxDescriptorType]? {
+        guard let descriptors = characteristic.descriptors else {
+            return nil
+        }
+        return descriptors.map {
+            RxCBDescriptor(descriptor: $0)
+        }
+    }
+    var service: RxServiceType {
+        return RxCBService(service: characteristic.service)
+    }
+    
+}
---
+++ Source/RxMutableCharacteristicType.swift
@@ -0,0 +1,64 @@
+//
+//  RxMutableCharacteristicType.swift
+//  RxBluetoothKit
+//
+//  Created by Andrew Breckenridge on 6/20/16.
+//  Copyright © 2016 Polidea. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+protocol RxMutableCharacteristicType {
+    
+    /// Characteristic UUID
+    var uuid: CBUUID { get }
+    
+    /// Current characteristic value
+    var value: NSData? { get }
+    
+    /// True if characteristic value changes are notified
+    var isNotifying: Bool { get }
+    
+    /// Characteristic properties
+    var properties: CBCharacteristicProperties { get }
+    
+    /// Characteristic descriptors
+    var descriptors: [RxDescriptorType]? { get }
+    
+    /// Characteristic service
+    var service: RxServiceType { get }
+    
+}
+
+/**
+ Characteristics are equal if their UUIDs are equal
+ 
+ - parameter lhs: First characteristic to compare
+ - parameter rhs: Second characteristic to compare
+ - returns: True if characteristics are the same
+ */
+func == (lhs: RxMutableCharacteristicType, rhs: RxMutableCharacteristicType) -> Bool {
+    return lhs.uuid == rhs.uuid
+}
+
+/**
+ Function compares if two characteristic arrays are the same, which is true if
+ both of them in sequence are equal and their size is the same.
+ 
+ - parameter lhs: First array of characteristics to compare
+ - parameter rhs: Second array of characteristics to compare
+ - returns: True if both arrays contain same characteristics
+ */
+func == (lhs: [RxMutableCharacteristicType], rhs: [RxMutableCharacteristicType]) -> Bool {
+    guard lhs.count == rhs.count else {
+        return false
+    }
+    var i1 = lhs.generate()
+    var i2 = rhs.generate()
+    var isEqual = true
+    while let e1 = i1.next(), e2 = i2.next() where isEqual {
+        isEqual = e1 == e2
+    }
+    return isEqual
+}
