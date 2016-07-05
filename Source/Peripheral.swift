56838ad6b5c11f5403d2a26a269197e1288abdfb
Kacper Harasim <kacper.harasim@polidea.com>
2016-07-05 15:35:27 +0200

    Fixed spacings in documentation

--- Source/Peripheral.swift
+++ Source/Peripheral.swift
@@ -122,8 +122,8 @@ public class Peripheral {
 	/**
 	 Triggers discover of specified services of peripheral. If the servicesUUIDs parameter is nil, all the available services of the
 	 peripheral are returned; setting the parameter to nil is considerably slower and is not recommended.
-     If all of the specified services are already discovered - these are returned without doing any underlying Bluetooth operations.
-     Next on returned `Observable` is emitted only when all of the requested services are discovered.
+	 If all of the specified services are already discovered - these are returned without doing any underlying Bluetooth operations.
+	 Next on returned `Observable` is emitted only when all of the requested services are discovered.
 
 	 - Parameter serviceUUIDs: An array of [CBUUID](https://developer.apple.com/library/ios/documentation/CoreBluetooth/Reference/CBUUID_Class/) objects that you are interested in. Here, each [CBUUID](https://developer.apple.com/library/ios/documentation/CoreBluetooth/Reference/CBUUID_Class/) object represents a UUID that identifies the type of service you want to discover.
 	 - Returns: Observable that emits `Next` with array of `Service` instances, once they're discovered.
@@ -206,7 +206,7 @@ public class Peripheral {
 	 Function that triggers characteristics discovery for specified Services and identifiers. Discovery is called after
 	 subscribtion to `Observable` is made.
 	 If all of the specified characteristics are already discovered - these are returned without doing any underlying Bluetooth operations.
- 	 Next on returned `Observable` is emitted only when all of the requested characteristics are discovered.
+	 Next on returned `Observable` is emitted only when all of the requested characteristics are discovered.
 
 	 - Parameter identifiers: Identifiers of characteristics that should be discovered. If `nil` - all of the
 	 characteristics will be discovered. If you'll pass empty array - none of them will be discovered.
