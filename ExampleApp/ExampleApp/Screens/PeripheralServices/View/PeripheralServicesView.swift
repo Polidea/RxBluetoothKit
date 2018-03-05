4dac326a1a728dd51b47b1356916821f7042ed85
Kamil <kamil@MacBook-Pro-Kamil.local>
2018-03-05 13:34:21 +0100

    Implement initial PeripheralServices Screen

---
+++ ExampleApp/ExampleApp/Screens/PeripheralServices/View/PeripheralServicesView.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class PeripheralServicesView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        addSubviews()
+        setupConstraints()
+        backgroundColor = .green
+    }
+
+    private func setupConstraints() {
+
+    }
+
+    private func addSubviews() {
+
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
