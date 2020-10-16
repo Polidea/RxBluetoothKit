5e48c4165fde7e618bf4e06b0b387f1e2d6ea24d
Mikołaj Piechocki <mikolaj.piechocki@polidea.com>
2020-10-16 11:55:55 +0200

    Remove old ExampleApp and add new one

---
+++ ExampleApp/ExampleApp/ExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Mikolaj Piechocki on 16/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
