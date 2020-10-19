0ae2ea371360417a199b211d30da5596f47460d2
Mikołaj Piechocki <mikolaj.piechocki@polidea.com>
2020-10-19 11:23:24 +0200

    Add connecting to peripheral

--- ExampleApp/ExampleApp/Screens/Central/CentralViewController.swift
+++ ExampleApp/ExampleApp/Screens/Central/CentralViewController.swift
@@ -1,3 +1,6 @@
+import CoreBluetooth
+import RxBluetoothKit
+import RxSwift
 import UIKit
 
 class CentralViewController: UIViewController {
@@ -14,6 +17,47 @@ class CentralViewController: UIViewController {
 
     override func loadView() {
         view = centralView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        centralView.connectButton.addTarget(self, action: #selector(handleConnectButton), for: .touchUpInside)
+    }
+
+    // MARK: - Private
+
+    private let disposeBag = DisposeBag()
+    private lazy var manager = CentralManager()
+
+    @objc private func handleConnectButton() {
+        guard let serviceUuidString = centralView.serviceUuidTextField.text,
+              let characteristicUuidString = centralView.characteristicUuidTextField.text else { return }
+
+        let serviceUuid = CBUUID(string: serviceUuidString)
+        let characteristicUuid = CBUUID(string: characteristicUuidString)
+
+        scanAndConnect(serviceUuid: serviceUuid, characteristicUuid: characteristicUuid)
+    }
+
+    private func scanAndConnect(serviceUuid: CBUUID, characteristicUuid: CBUUID) {
+        let managerIsOn = manager.observeStateWithInitialValue()
+            .filter { $0 == .poweredOn }
+            .map { _ in }
+
+        Observable.combineLatest(managerIsOn, Observable.just(manager)) { $1 }
+            .flatMap { $0.scanForPeripherals(withServices: [serviceUuid]) }
+            .take(1)
+            .flatMap { $0.peripheral.establishConnection() }
+            .flatMap { $0.discoverServices([serviceUuid]) }
+            .take(1)
+            .compactMap { $0.first }
+            .flatMap { $0.discoverCharacteristics([characteristicUuid]) }
+            .take(1)
+            .compactMap { $0.first }
+            .flatMap { $0.readValue() }
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
     }
 
 }
