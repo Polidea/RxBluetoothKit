94daf4212ae94639cad63932373aecc89f8f68b9
Chamira Fernando <chamira.fernando@airthings.com>
2023-03-07 12:40:57 +0100

    Update Cartfile, Package.swift and RxBluetoothKit_Airthings.podspec with RxSwift 6.5

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "RxBluetoothKit_Airthings", targets: ["RxBluetoothKit_Airthings"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.1.1"))
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.5"))
     ],
     targets: [
         .target(
