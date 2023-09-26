00a05286120efca186f58f01a4ca438c928b3aea
86 <774945+86@users.noreply.github.com>
2023-09-26 16:48:28 +0900

    chore: iOS 17 support (#2)
    * fix: resolve name conflict between RxSwift and Apple.Observation framework

--- Source/Unimplemented.swift
+++ Source/Unimplemented.swift
@@ -5,10 +5,10 @@ func unimplementedFunction(file: String = #file, function: String = #function, l
     fatalError("Unimplemented function \(function) in \(file):\(line)")
 }
 
-extension Observable {
+extension RxSwift.Observable {
     static func unimplemented(file: String = #file, function: String = #function, line: Int = #line)
-        -> Observable<Element> {
+    -> RxSwift.Observable<Element> {
         unimplementedFunction(file: file, function: function, line: line)
-        return Observable<Element>.empty()
+        return RxSwift.Observable<Element>.empty()
     }
 }
